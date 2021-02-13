64622e0844d2d4c0bd2a15f5b7c52554f5688a82
rubo92i <ruben.manukyan@appish.co.uk>
2021-02-13 12:56:50 +0400

    init

---
+++ src/am/basic/jdbc/Main.java
@@ -0,0 +1,21 @@
+package am.basic.jdbc;
+
+import am.basic.jdbc.model.User;
+import am.basic.jdbc.repository.UserRepository;
+import am.basic.jdbc.repository.impl.UserRepositoryImpl;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        UserRepository userRepository = new UserRepositoryImpl();
+        User user = userRepository.getByUsername("ruben.manukyan");
+        System.out.println(user);
+        user.setPassword("newPassword");
+        userRepository.update(user);
+
+        user = userRepository.getByUsername("ruben.manukyan");
+
+        System.out.println(user);
+    }
+}
---
+++ src/am/basic/jdbc/model/User.java
@@ -0,0 +1,67 @@
+package am.basic.jdbc.model;
+
+public class User {
+
+    private long id;
+
+    private String name;
+
+    private String surname;
+
+    private String username;
+
+    private String password;
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
